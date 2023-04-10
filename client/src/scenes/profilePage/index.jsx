db273c4d8465e7b36d3e9346700a8267ecf25c87
unknown <joms.madhousetech@gmail.com>
2023-04-10 10:17:44 +0100

    Testing remote service hosts.. will remove later...

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -15,7 +15,9 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+    console.log(process.env.SERVER_URL);
+    //const response = await fetch(`http://localhost:3001/users/${userId}`, {
+    const response = await fetch(`https://admin-backend-bdrj.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
